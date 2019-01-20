e9ef74166bbab904babaa9c8bed89245445fc329
Clint.Network <me@clint.network>
2019-01-20 17:34:31 +0100

    Adding TypeDBValueException

---
+++ Exceptions/TypeDBValueException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TypeDB
+{
+    [Serializable]
+    internal class TypeDBValueException : Exception
+    {
+        public TypeDBValueException()
+        {
+        }
+
+        public TypeDBValueException(string message) : base(message)
+        {
+        }
+
+        public TypeDBValueException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected TypeDBValueException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
